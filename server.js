9ff9c49da49da78e9a40733bd87cd8594a774798
ashishpatel46 <ashishkp421@gmail.com>
2026-04-15 16:09:52 +0530

    Refactor server.js to improve connection handling
    Removed unused middleware for database connection and updated server start logic.

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 import app from "./app.js";
+import connection from "./database/connection.js";
 import cloudinary from "cloudinary";
 
+// DB connect
+await connection();
+
+// Cloudinary config
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use((req, res, next) => {
-  if (!isConnected) {
-    connection();
-  }
-  next();
-});
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server listening on port ${process.env.PORT}`);
 // });
+
+//  Run server locally
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+//  Export for Vercel
 export default app;
